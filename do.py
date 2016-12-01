a9264843d8fc787baa7cd1cad496d550207d010b
root <root@schonefrau.com>
2016-12-01 01:40:55 +0000

    Saving progress so far

--- do.py
+++ do.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python
 
-print "Welcome to Digital Ocean Tools :) "
+import os
 
+# Determines if the operating system is debian
+def isDebian():
+	return  os.path.isfile("/etc/debian_version")
+
+
+if __name__ == "__main__":
+	print "Welcome to Digital Ocean Tools  "
+
+	if isDebian():
+		print "You are using debian"
+	else:
+		print "You are using redhat"
--- setup.sh
+++ setup.sh
@@ -2,17 +2,27 @@
 
 echo "Welcome to DigitalOcean-Tools!"
 
-if [ -f "/etc/debian_version" ];
+if [ -f "/etc/debian_version" ]
+then
   debian=1
 else
   debian=0
 fi
 
+wget https://raw.githubusercontent.com/LagrangianPoint/DigitalOcean-Tools/master/do.py
+chmod +x do.py
+
 if [ $debian -eq 1 ]
 then
 
-sudo apt-get update
-sudo apt-get install git
-sudo apt-get install python
-
+	# Installing Git and Python for using do.py
+	sudo apt-get update
+	sudo apt-get install git
+	sudo apt-get install python
+else
+	# Installing Git and Python for using do.py
+	sudo yum install git
+	sudo yum install python
 fi
+
+
